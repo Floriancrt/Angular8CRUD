0d1e7578ac7967cb303bdc8d2513e8d783a0cb2f
Floriancrt <florian.courthieu@gmail.com>
2019-09-20 17:57:18 +0200

    UPD: Create Route And Model Files

--- api/DB.js
+++ api/DB.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
-    DB_PASS: 'mongodb+srv://FlorianC:UmBcqcRLSIjRSFtu@clusterfc-rrveo.mongodb.net/test?retryWrites=true&w=majority'
+
+    DB: process.env.DB_PASS
 };
+
